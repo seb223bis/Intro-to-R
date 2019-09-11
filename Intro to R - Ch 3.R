007b27fe848260ca25ef82af0a34389246c98c0d
Sarah <seb223@lehigh.edu>
2019-09-11 19:22:47 -0400

    Added name

--- Intro to R - Ch 3.R
+++ Intro to R - Ch 3.R
@@ -1,6 +1,6 @@
 ############################################################
 # R script to accompany Intro to R for Business, Chapter 3 #
-# Written by Troy Adair                                    #
+# Written by Troy Adair. Copy for seb223                                    #
 ############################################################
 
 # Still nothing to see here
