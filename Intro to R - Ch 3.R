874aea9ef048fe16aaeb1cbfec0c2cd798fe0f44
Sarah <seb223@lehigh.edu>
2019-09-11 19:13:27 -0400

    Revert "Added my name"
    This reverts commit 0fa2ec2b0e980b2cc5f19081f4cb352012fc0cea.

--- Intro to R - Ch 3.R
+++ Intro to R - Ch 3.R
@@ -1,6 +1,6 @@
 ############################################################
 # R script to accompany Intro to R for Business, Chapter 3 #
-# Written by Troy Adair. Copy for seb223                                    #
+# Written by Troy Adair                                    #
 ############################################################
 
 # Still nothing to see here
